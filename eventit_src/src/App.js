9b712d1573c717d1dff6938f703c9c2749a404ae
Liam Sexton <liams4@uw.edu>
2020-01-06 12:15:51 -0800

    Update header

--- eventit_src/src/App.js
+++ eventit_src/src/App.js
@@ -1,31 +1,17 @@
 import React from 'react';
+import InputBar from './InputBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Eventertons</h1>
-        <p>Welcome to Eventertons, possibly the best app in the whole world.</p>
+        <h1>Eventit</h1>
+        <h2>Welcome to Eventit! Eventit helps you find cool events nearby to you.</h2>
       </header>
-    </div>
-    <div>
-
+      <InputBar/>
     </div>
   );
 }
 
 export default App;
-
-// basically the goal of the app is to suggest some events to a user based on their city.
-
-// we could have a bar saying please enter your city - unfortunately not all locations are supported. we could have a csv file of all of the cities for which we read from.
-// could have an autocomplete thing like from 373, or just a general fill thing where we
-// only display like 5 cities at a time. 
-
-// buttons of the different events to suggest, with an option to type in your own.
-
-// ask for the user's location and then get directions to the event, or maybe allow the 
-// user to enter a location and get directions for there.
-// we could start with just a get directions link that opens to google maps, or there
-// may be an easy way for the user to just enter their location into google maps
